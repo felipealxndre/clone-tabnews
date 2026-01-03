cb7686f61d1c3bd60e3e613a88230f55e598d90a
felipealxndre <fealp@BOOK-15P9SPLLR5.localdomain>
2026-01-03 01:00:49 -0300

    add  file

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.POSTGREES_HOST,
-    port: process.env.POSTGREES_PORT,
-    user: process.env.POSTGREES_USER,
-    database: process.env.POSTGREES_DATABASE,
-    password: process.env.POSTGREES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
   const result = await client.query(queryObject);
