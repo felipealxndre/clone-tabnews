c27cd387b09f322a426da155c4f88b238df2ca90
felipealxndre <fealp@BOOK-15P9SPLLR5.localdomain>
2026-01-10 16:09:35 -0300

    adds `/api/v1/migrations` endpoint

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,19 @@
+import database from "infra/database.js";
+beforeAll(cleanDatabase);
+
+// derruba o schema principal
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("GET /api/v1/migrations deveria retornar status 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  console.log(responseBody);
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
